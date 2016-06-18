3bc94fc0339146fcffd0bcd2e795fae83bdd440f
李林 <ww1095@163.com>
2016-06-18 10:50:27 +0800

    修复微信朋友圈无法分享的BUG： 原因是ShareSDK.xml配置错误，需要配置三处

--- src/com/andli/sharedemo/MainActivity.java
+++ src/com/andli/sharedemo/MainActivity.java
@@ -31,10 +31,8 @@ public class MainActivity extends Activity {
 		OnekeyShare oks = new OnekeyShare();
 		// 关闭sso授权
 		oks.disableSSOWhenAuthorize();
-
 		// 分享时Notification的图标和文字 2.5.9以后的版本不调用此方法
-		// oks.setNotification(R.drawable.ic_launcher,
-		// getString(R.string.app_name));
+		// oks.setNotification(R.drawable.ic_launcher,getString(R.string.app_name));
 		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
 		oks.setTitle("微信分享");
 		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
@@ -42,7 +40,7 @@ public class MainActivity extends Activity {
 		// text是分享文本，所有平台都需要这个字段
 		oks.setText("我是分享文本");
 		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-		// oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+		//oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
 		// url仅在微信（包括好友和朋友圈）中使用
 		oks.setUrl("http://sharesdk.cn");
 		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
@@ -51,7 +49,6 @@ public class MainActivity extends Activity {
 		oks.setSite(getString(R.string.app_name));
 		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
 		oks.setSiteUrl("http://sharesdk.cn");
-
 		// 启动分享GUI
 		oks.show(this);
 	}
